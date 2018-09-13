c52b3cba70d8fe67a7b5335e7631293ef9641750
Erik Porsche <e.porsche@gmail.com>
2018-09-13 09:05:47 +0200

    test trans

--- src/app/code/community/Fontis/Blog/Block/Abstract.php
+++ src/app/code/community/Fontis/Blog/Block/Abstract.php
@@ -53,7 +53,7 @@ abstract class Fontis_Blog_Block_Abstract extends Mage_Core_Block_Template
 
         // Check if we need to use summary content and make adjustments as necessary
         if (Mage::getStoreConfig("fontis_blog/blog/usesummary") && ($summaryContent = $post->getSummaryContent())) {
-            $summaryContent .= ' ...&nbsp;&nbsp;<a class="readMore" href="' . $post->getAddress() . '">' . $this->__("Read More") . '</a>';
+            $summaryContent .= ' ...&nbsp;&nbsp;<a class="readMore" href="' . $post->getAddress() . '">' . $this->__("mehr...") . '</a>';
             $post->setPostContent($summaryContent);
         } else if ($readMore = (int)Mage::getStoreConfig('fontis_blog/blog/readmore')) {
             $content = $post->getPostContent();
@@ -61,7 +61,7 @@ abstract class Fontis_Blog_Block_Abstract extends Mage_Core_Block_Template
                 $content = substr($content, 0, $readMore);
                 $content = substr($content, 0, strrpos($content, '. ') + 1);
                 $content = $this->closeTags($content);
-                $content .= ' ...&nbsp;&nbsp;<a class="readMore" href="' . $post->getAddress() . '">' . $this->__("Read More") . '</a>';
+                $content .= ' ...&nbsp;&nbsp;<a class="readMore" href="' . $post->getAddress() . '">' . $this->__("mehr...") . '</a>';
             }
             $post->setPostContent($content);
         }
